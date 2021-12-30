23ea084f24f947073a7717a46aa41a21c7ac7a23
urth1g <djolecs97@gmail.com>
2021-12-30 14:05:30 +0100

    changes

--- db/db.js
+++ db/db.js
@@ -9,8 +9,9 @@ class Database {
 		this.db = mysql.createPool({
 		  host     : process.env.DB_HOST,
 		  user     : process.env.DB_USER,
-		  password : '',
+		  password : process.env.DB_PASSWORD || '',
 		  database : process.env.DB_NAME,
+		  port : process.env.DB_PORT || 3306,
 		  multipleStatements: true,
 		  waitForConnections: true,
 		  connectionLimit: 10,

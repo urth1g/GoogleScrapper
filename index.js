1a8c2b43fd4fb4cb0955f71851e5c3244c4cd090
Djordje Jevremovic <djolecs97@gmailcom>
2022-04-26 03:54:59 +0200

    Add environment variables

--- index.js
+++ index.js
@@ -1040,7 +1040,7 @@ app.post("/check_for_good_deals", async (req,resp) => {
 	let { matnr } = req.body;
 
 	console.log('checking for good deals');
-	let { data } = await axios.post('http://localhost:8080/admin/get_inventory', {Matnr: matnr})
+	let { data } = await axios.post(`${process.env.BACKEND_ENDPOINT_URL}/admin/get_inventory`, {Matnr: matnr})
 	let response = await axios.post('http://localhost:8080/log_info', {matnr})
 
 	let page4Link = "https://google.com" + response.data[0].Link

fa052298fc0d92be07fedcf0aa6830c2ee5a2411
Djordje Jevremovic <djolecs97@gmailcom>
2022-07-16 09:45:28 +0200

    Add limit to avoid memory leak

--- data_processing/removeDuplicates.js
+++ data_processing/removeDuplicates.js
@@ -1,29 +1,21 @@
 const Database = require('../db/db')
 
+
+const timer = ms => new Promise( resolve => setTimeout(resolve, ms))
 async function removeDuplicates(class1, subclass){
-    let res = await Database.makeQuery2("SELECT ShortName, LongName, Matnr FROM products WHERE Class LIKE '%" + class1 + "%' AND SubClass LIKE '%" + subclass + "%';")
+    let res = await Database.makeQuery2("SELECT * FROM products INNER JOIN models_information ON products.Matnr = models_information.Matnr WHERE SubClass LIKE '%Rack%';")
 
-    let dict = {}
-    let duplicates = []
+    console.log(res.length)
+
     for(let r of res){
-        let matnr = r.Matnr
-        let ShortName = r.ShortName.split(" - ")[0]
-        if(!dict[ShortName]){
-            dict[ShortName] = matnr;
-        }else{
-            duplicates.push({duplicateMatnr: matnr, duplicateOf: dict[ShortName]})
-        }
-    }
 
-    for(let d of duplicates){
-        let res = await Database.makeQuery2("DELETE FROM products WHERE Matnr = ? LIMIT 1", [d.duplicateMatnr])
-        console.log(res)
+        let queryResult = await Database.makeQuery2("DELETE FROM models_information WHERE Matnr = ?", [r.Matnr])
+        console.log(queryResult)
+        await timer(5)
     }
 }
 
 let c = process.argv[2]
 let sc = process.argv[3]
 
-if(sc){
-    removeDuplicates(c, sc)
-}
+removeDuplicates()
